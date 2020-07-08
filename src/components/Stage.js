af03432106d9640d06d0c394bb73be6baa2a6547
Mosa M.Sebdani <msebdani@gmail.com>
2020-07-08 01:41:58 -0700

    add styling to all components

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -4,7 +4,10 @@ import { StyledStage } from './styles/StyledStage';
 
 
 const Stage = ({ stage }) => (
-  <StyledStage>
+  <StyledStage
+    width={stage[0].length}
+    height={stage.length}
+  >
     { stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} /> ))}
 
   </StyledStage>
